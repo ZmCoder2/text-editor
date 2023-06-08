d4f2a9909db3f455934b0b85e8398542a2d9e198
Zack Miller <126508376+ZmCoder2@users.noreply.github.com>
2023-06-07 21:23:07 -0400

    client/src/js/index.js

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -13,9 +13,27 @@ const initdb = async () =>
   });
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
-
+export const putDb = async (content) => {
+console.log('PUT to the database');
+const jate = await openDB('jate', 1);
+const text = jate.transaction('jate', 'readwrite');
+const store = text.objectStore('jate');
+const request = store.put({ id: 1, value: content });
+const result = await request;
+console.log('Data saved to database!', result.value);
+};
 // TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+export const getDb = async () => {
+  cconsole.log('GET from database');
+  const jate = await openDB('jate', 1);
+  const text = jate.transaction('jate', 'readonly');
+  const store = text.objectStore('jate');
+  const request = store.get(1);
+  const result = await request;
+  result
+    ? console.log('Data retrieved from the database', result.value)
+    : console.log('Data not found in the database.');
 
+  return result?.value;
+};
 initdb();
